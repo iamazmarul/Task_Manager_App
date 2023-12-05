7aa7b57863b0a3c70498c5a691336efe52cc4029
Azmarul Islam <imjohnny.dev@gmail.com>
2023-12-05 22:34:01 +0600

    Complete Task Manager Project

--- lib/ui/widgets/profile_summary_card.dart
+++ lib/ui/widgets/profile_summary_card.dart
@@ -8,9 +8,10 @@ import 'package:task_manager/ui/screens/update_profile_screen.dart';
 
 class ProfileSummaryCard extends StatefulWidget {
   const ProfileSummaryCard({
-    super.key,
+    Key? key,
     this.enableOnTab = true,
-  });
+  }) : super(key: key);
+
   final bool enableOnTab;
 
   @override
@@ -18,35 +19,56 @@ class ProfileSummaryCard extends StatefulWidget {
 }
 
 class _ProfileSummaryCardState extends State<ProfileSummaryCard> {
+  Uint8List? imageBytes;
+
+  @override
+  void initState() {
+    super.initState();
+    loadImage();
+  }
+
+  void loadImage() {
+    try {
+      String? photoData = AuthenticationController.user?.photo;
+      if (photoData != null && photoData.isNotEmpty) {
+        imageBytes = const Base64Decoder().convert(photoData);
+      } else {
+        imageBytes = null;
+      }
+    } catch (e) {
+      imageBytes = null;
+    }
+  }
+
+  @override
+  void didUpdateWidget(covariant ProfileSummaryCard oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    loadImage();
+  }
+
   @override
   Widget build(BuildContext context) {
-    Uint8List imageBytes;
-    try {
-      imageBytes = const Base64Decoder()
-          .convert(AuthenticationController.user?.photo ?? '');
-    } catch (e) {
-      imageBytes = Uint8List(0);
-    }
-
     return ListTile(
       onTap: () {
         if (widget.enableOnTab) {
           Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => const UpdateProfileScreen()));
+            context,
+            MaterialPageRoute(
+              builder: (context) => const UpdateProfileScreen(),
+            ),
+          );
         }
       },
       leading: CircleAvatar(
-        child: AuthenticationController.user?.photo == null
-            ? const Icon(Icons.person)
-            : ClipRRect(
-                borderRadius: BorderRadius.circular(30),
-                child: Image.memory(
-                  imageBytes,
-                  fit: BoxFit.cover,
-                ),
-              ),
+        child: imageBytes != null
+            ? ClipRRect(
+          borderRadius: BorderRadius.circular(30),
+          child: Image.memory(
+            imageBytes!,
+            fit: BoxFit.cover,
+          ),
+        )
+            : const Icon(Icons.person),
       ),
       title: Text(
         FullName,
@@ -67,9 +89,10 @@ class _ProfileSummaryCardState extends State<ProfileSummaryCard> {
           await AuthenticationController.clearAuthData();
           if (mounted) {
             Navigator.pushAndRemoveUntil(
-                context,
-                MaterialPageRoute(builder: (context) => const LoginScreen()),
-                (route) => false);
+              context,
+              MaterialPageRoute(builder: (context) => const LoginScreen()),
+                  (route) => false,
+            );
           }
         },
         icon: const Icon(Icons.logout_outlined),
